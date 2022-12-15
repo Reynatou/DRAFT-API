38bf9f8d8e9f15427a2986371440ca7a5e4d3a0c
Reynatou <sreynaf@yahoo.com>
2022-12-15 15:37:38 +0100

    new

--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
-/*Création de la constante APIKEY et affectatioh de la valeur de la clé API*/
-const APIKEY = `dfd0cf24f3a6ffc667dfda89950271fa`;
-
-/*1er appel à l'API, formatage des informations en format json*/
+/*Appel à l'API, formatage des informations en format json*/
 /*Nouvelle promesse pour récupérer les données et affichage des données en modifiant les pages HTML*/
 /*Appel de l'API avec ville en paramètre, affectation de l'url de l'API*/
-
 let apiCall = function (city) {
 
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric&lang=fr`;
@@ -13,7 +9,7 @@ let apiCall = function (city) {
     .then((data) => {
         console.log(data);
         document.querySelector(`#city`).innerHTML = data.name;
-        document.querySelector(`#temp`).innerHTML = "<i class='fas fa-thermometer-half'></i>" + data.main.temp + `°`;
+        document.querySelector(`#temp`).innerHTML = "<i class='fas fa-thermometer-half'></i>" + data.main.temp + `°C`;
         document.querySelector(`#humidity`).innerHTML = "<i class='fas fa-tint'></i>" + data.main.humidity + `%`;
         document.querySelector(`#wind`).innerHTML = "<i class='fas fa-wind'></i>" + data.wind.speed + `km/h`;
     }))
@@ -22,22 +18,51 @@ let apiCall = function (city) {
 .catch((err) => console.log(`Erreur :` + err));
 };
 
-/*Ecouteur d'événement sur la soummission du formulaire, cassage de l'appel par défaut*/
-/*Récupération du nom de la ville entré à partir de la page HTML et appel de l'API*/
 
+let form = document.querySelector(`#searchForm`);
 
-document.querySelector(`form`).addEventListener(`submit`, function (e) {
-    let ville = document.querySelector(`#inputCity`).value;
+/* Ecoute de la modification du nom de ville */
+
+form.nameVille.addEventListener(`change`, function() {
+    validName(this);
+});
+
+/* Ecoute de la soumission du formulaire */
+form.addEventListener(`submit`, function(e) {
     e.preventDefault();
-    apiCall(ville);
-  }
-);
+    if (validName(form.nameVille)) {
+        apiCall(form.nameVille.value);
+    };
+});
+//**************** Validation nom de ville ******************
+const validName = function(inputName) {
+    //Création de la reg exp pour validation du nom de ville
+    let nameRegExp = new RegExp(
+        '^[a-zA-Z -]+$', 'g'
+    );
+   
+        let testNameVille = nameRegExp.test(inputName.value);
+        //Affichage
+        //Récupération de la balise SMALL
+        let small = inputName.nextElementSibling;
 
-/*Apple par défaut lors du chargement de la page*/
+    if (testNameVille) {
+        small.innerHTML = "Nom de ville valide";
+        small.classList.remove('text-danger');
+        small.classList.add('text-success');
+        return true;
+    }
+    else {
+        small.innerHTML = "Nom de ville non valide";
+        small.classList.remove('text-success');
+        small.classList.add('text-danger');
+        return false;
+    }
+};
+
+/*Création de la constante APIKEY et affectation de la valeur de la clé API*/
+const APIKEY = `dfd0cf24f3a6ffc667dfda89950271fa`;
+
+/*Appel par défaut lors du chargement de la page*/
 
 apiCall(`Paris`);
-
-
-
-
-
